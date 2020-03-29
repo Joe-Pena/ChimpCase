a30e9eba39025266c9ad8e13409dba237045816d
Joe Pena <joe@joepena.io>
2020-03-28 22:48:03 -0600

    Add title support

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 chimpCase = (
   str,
   options = {
-    title: false,
-    lowerCase: false,
-    upperCase: false,
-    case: "camelCase"
+    format: "default",
+    case: "camelCase",
+    exception: null
   }
 ) => {
-  const firstLetter = str[0];
-  const restOfWord = str.slice(1).toLowerCase();
+  if (!str || typeof str !== "string") {
+    throw new TypeError(
+      "Arguments passed to function chimpCase are invalid or empty."
+    );
+  }
 
-  return firstLetter + restOfWord;
+  str.trim();
+  const firstLetter = str[0].toUpperCase();
+  let restOfString = str
+    .substring(1)
+    .replace(/([-|_])/g, " ")
+    .toLowerCase();
+
+  if (options.format === "title") {
+    const wordList = restOfString.split(" ").map((word, index) => {
+      const firstLetter = index !== 0 ? word[0].toUpperCase() : word[0];
+      const restOfWord = word.substring(1);
+
+      return firstLetter + restOfWord;
+    });
+
+    restOfString = wordList.join(" ");
+  }
+
+  return firstLetter + restOfString;
 };
 
+console.log(chimpCase("BEST-Type_of_word_the movie", { format: "title" }));
 module.exports.chimpCase = chimpCase;
